f73bc7a71377b6f6815ca01ce0ba2276ae8e92dd
Cameron Fairlie <camerondjfairlie@gmail.com>
2022-07-17 21:19:25 +1000

    GUI-Test, Prints Rectangle when button pressed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Simple Python Area Calculator
-A simple area calculator built in Python.
-It takes two numbers and returns the area of a rectangle, the area of a triangle, the area of a circle, or the area of a trapezoid.
+## Simple Python Area Calculator (GUI-Test)
+GUI test for main branch
+
+[]: # Language: python
+[]: # Path: main.py
--- main.py
+++ main.py
@@ -1,80 +1,25 @@
 from math import pi
 from os import name, system
 from time import sleep
+import tkinter as tk
+import tkinter.ttk as ttk
 
 
-# Calc Functions
-def rectangle():
-    global width, length
-    try:
-        # Requests user input
-        width = float(input("Input Width: "))
-        length = float(input("Input Length: "))
-    # If a non-float value is inputted throws error and reruns function
-    except ValueError:
-        print('\033[91m' + "Value is not a number" + '\033[0m')
-        rectangle()
-    return width * length
-
-
-def triangle():
-    global height, base
-    try:
-        # Requests user input
-        height = int(input("Input Height: "))
-        base = int(input("Input Base: "))
-    # If a non-float value is inputted throws error and reruns function
-    except ValueError:
-        print('\033[91m' + "Value is not a number" + '\033[0m')
-        triangle()
-    return 0.5 * height * base
-
-
-def trapezium():
-    global top, bottom, height
-    # Requests user input
-    try:
-        top = int(input("Input Top: "))
-        bottom = int(input("Input Bottom: "))
-        height = int(input("Input Height: "))
-    # If a non-float value is inputted throws error and reruns function
-    except ValueError:
-        print('\033[91m' + "Value is not a number" + '\033[0m')
-        trapezium()
-    return 0.5 * (top + bottom) * height
-
-
-def circle():
-    global radius
-    # Requests user input
-    try:
-        radius = int(input("Input Radius: "))
-    # If a non-float value is inputted throws error and reruns function
-    except ValueError:
-        print('\033[91m' + "Value is not a number" + '\033[0m')
-
-    return pi * radius ** 2
+def btnRectangleDown():
+    print("Rectangle")
 
 
 def main():
-    # User input
-    shape = str(input("Input Shape Type (Rectangle/Triangle/Trapezium/Circle): ").lower())
+    root = tk.Tk()
+    root.title("Area Calculator")
+    root.geometry("300x300")
 
-    # Call function
-    if shape == "rectangle":
-        print(rectangle())
-    elif shape == "triangle":
-        print(triangle())
-    elif shape == "trapezium":
-        print(trapezium())
-    elif shape == "circle":
-        print(circle())
-    else:
-        # anything not in the list throws error and reruns function
-        print('\033[91m' + "INVALID" + '\033[0m')
-        sleep(0.5)
-        system('cls' if name == 'nt' else 'clear')
-        main()
+    btnframe = ttk.Frame(root)
+    rectangle_btn = ttk.Button(btnframe, text="Rectangle", command=btnRectangleDown)
+    rectangle_btn.grid(column=1, row=0)
+    btnframe.grid()
+
+    root.mainloop()
 
 
 # checks that program is not running as library then runs main function
